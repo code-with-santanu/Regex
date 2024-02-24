bd8833bd3e645a89c0855128c1ee2b8d8e58b104
code-with-santanu <santanupal003@gmail.com>
2024-02-25 02:07:02 +0530

    simplematch1 added

---
+++ Regex_in_python/SimpleMatching1.py
@@ -0,0 +1,15 @@
+'''
+Task:
+To locate the character or word you're searching for, just type it directly, much like a regular search. For instance, if you're looking for the word curious in the text, enter it exactly as is.  
+'''
+
+# Import the necessary library
+import re
+
+text = "I have no special talents.I am only passionately curious."
+
+# Use search function to search for the literal word "curious"
+match = re.search(r"curious", text)
+
+# Printing match
+print(match)
